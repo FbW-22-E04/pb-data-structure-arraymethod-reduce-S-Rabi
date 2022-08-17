50b4d87675a99032b6c37ae42a0e907bf028a4b3
S Rabi <rabi.sanaye@yahoo.com>
2022-08-17 16:37:38 +0200

    c

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-
 //Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
 //1
 
+const { Value } = require("sass");
+
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
 function groupById(array) {
   //write your code here
 }
 
-let usersById= groupById(users)
-console.log(usersById)
+// let usersById = groupById(users);
+// console.log(usersById);
 
 /* expected output: 
 
@@ -24,3 +25,12 @@ console.log(usersById)
 }
   
 */
+
+function groupById(array) {
+  return array.reduce((obj, Value) => {
+    obj[Value.id] = Value;
+    return obj;
+  }, {});
+}
+let usersById2 = groupById(users);
+console.log(usersById2);
